455f6726597d9eb94e84da0a38807c94fbf3fc02
Metroynome <yort124@gmail.com>
2022-03-24 23:22:49 -0400

    Updated Code to use Assembly instead.

--- dpad-camera-speed/main.c
+++ dpad-camera-speed/main.c
@@ -4,30 +4,51 @@
 #include <libdl/player.h>
 #include <libdl/pad.h>
 
-int CameraSpeed = 0x100;
 
 void DPadCameraLogic(void)
 {
-	int CameraSpeedAddr = 0x00171F18;
-	int num = 0;
-	if ((*(u16*)0x001EE682 & PAD_LEFT) == 0)
-	{
-		num = -0x8;
-	}
-	else if ((*(u16*)0x001EE682 & PAD_RIGHT) == 0 | (*(u16*)0x001EE682 & PAD_CROSS) == 0)
-	{
-		num = 0x8;
-	}
-	int NewSpeed = (u32)(*(u32*)CameraSpeedAddr + num);
-	if (NewSpeed >= CameraSpeed)
-	{
-		NewSpeed = CameraSpeed;
-	}
-	else if (NewSpeed <= 0)
-	{
-		NewSpeed = 0;
-	}
-	*(u32*)CameraSpeedAddr = NewSpeed;
+	asm (
+		// Load Pad 1
+		"lui	$t6, 0x001F\n"
+		"addiu 	$t6, $t6, 0xE682\n"
+		"lh	$t6, 0x0($t6)\n"
+		// Load Jokers
+		"addiu 	$t7, $zero, 0xFF7F\n" // Left
+		"addiu 	$t8, $zero, 0xFFDF\n" // Right
+		"addiu 	$t9, $zero, 0xBFFF\n" // Cross
+		// if d-pad Left, Subtract
+		"addiu 	$t5, $zero, -0x8\n"
+		"beq 	$t6, $t7, _DoNewSpeed\n"
+		// if d-pad Right, Add
+		"addiu 	$t5, $zero, 0x8\n"
+		"beq 	$t6, $t8, _DoNewSpeed\n"
+		//if Cross, Add
+		"addiu 	$t5, $zero, 0x8\n"
+		"beq 	$t6, $t9, _DoNewSpeed\n"
+		// if Nothing is pressed, exit.
+		"nop;"
+		"beq	$zero, $zero, _exitDpadCamera;"
+
+		"_DoNewSpeed:"
+		"add 	$t5, $t5, $v0\n"
+		// if less than zero, save Zero (Can save Max Speed to Cycle Around)
+		"slti	$t6, $t5, 0xffff\n"
+		"sw		$zero, 0x0($a0)\n"
+		"bne 	$t6, $zero, _exitDpadCamera\n"
+
+		// if greater than max speed, save Max Speed
+		"slt 	$t6, $a2, $t5\n"
+		"sw 	$a2, 0x0($a0)\n"
+		"bne 	$t6, $zero, _exitDpadCamera\n"
+		
+		// Everything In Between
+		"sw 	$t5, 0x0($a0)\n"
+		
+		"_exitDpadCamera:"
+		"jr 	$ra\n"
+		"nop\n"
+	);
+}
 }
 
 int main(void)
@@ -54,8 +75,6 @@ int main(void)
 			*(u32*)0x00561BCC = 0x0C000000 | ((u32)(&DPadCameraLogic) / 4);
 			*(u32*)0x00561BD0 = 0;
 			*(u32*)0x00561BD8 = 0;
-			*(u32*)0x00561C1C = 0x3C050017;
-			*(u32*)0x00561C20 = 0x8CA21F18;
 		}
 	}
 	return 0;
