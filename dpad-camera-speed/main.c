19bb23bce8ca65edca2305adec8972934635a466
Metroynome <yort124@gmail.com>
2022-03-24 23:43:52 -0400

    Updated Camera Logic again.

--- dpad-camera-speed/main.c
+++ dpad-camera-speed/main.c
@@ -9,44 +9,44 @@ void DPadCameraLogic(void)
 {
 	asm (
 		// Load Pad 1
-		"lui	$t6, 0x001F\n"
-		"addiu 	$t6, $t6, 0xE682\n"
-		"lh	$t6, 0x0($t6)\n"
+		"lui	$t6, 0x001F;"
+		"addiu 	$t6, $t6, 0xE682;"
+		"lh	$t6, 0x0($t6);"
 		// Load Jokers
-		"addiu 	$t7, $zero, 0xFF7F\n" // Left
-		"addiu 	$t8, $zero, 0xFFDF\n" // Right
-		"addiu 	$t9, $zero, 0xBFFF\n" // Cross
+		"addiu 	$t7, $zero, 0xFF7F;" // Left
+		"addiu 	$t8, $zero, 0xFFDF;" // Right
+		"addiu 	$t9, $zero, 0xBFFF;" // Cross
 		// if d-pad Left, Subtract
-		"addiu 	$t5, $zero, -0x8\n"
-		"beq 	$t6, $t7, _DoNewSpeed\n"
+		"addiu 	$t5, $zero, -0x8;"
+		"beq 	$t6, $t7, _DoNewSpeed;"
 		// if d-pad Right, Add
-		"addiu 	$t5, $zero, 0x8\n"
-		"beq 	$t6, $t8, _DoNewSpeed\n"
+		"addiu 	$t5, $zero, 0x8;"
+		"beq 	$t6, $t8, _DoNewSpeed;"
 		//if Cross, Add
-		"addiu 	$t5, $zero, 0x8\n"
-		"beq 	$t6, $t9, _DoNewSpeed\n"
+		"addiu 	$t5, $zero, 0x8;"
+		"beq 	$t6, $t9, _DoNewSpeed;"
 		// if Nothing is pressed, exit.
 		"nop;"
 		"beq	$zero, $zero, _exitDpadCamera;"
 
 		"_DoNewSpeed:"
-		"add 	$t5, $t5, $v0\n"
+		"add 	$t5, $t5, $v0;"
 		// if less than zero, save Zero (Can save Max Speed to Cycle Around)
-		"slti	$t6, $t5, 0xffff\n"
-		"sw		$zero, 0x0($a0)\n"
-		"bne 	$t6, $zero, _exitDpadCamera\n"
+		"slti	$t6, $t5, 0xffff;"
+		"sw		$zero, 0x0($a0);"
+		"bne 	$t6, $zero, _exitDpadCamera;"
 
 		// if greater than max speed, save Max Speed
-		"slt 	$t6, $a2, $t5\n"
-		"sw 	$a2, 0x0($a0)\n"
-		"bne 	$t6, $zero, _exitDpadCamera\n"
+		"slt 	$t6, $a2, $t5;"
+		"sw 	$a2, 0x0($a0);"
+		"bne 	$t6, $zero, _exitDpadCamera;"
 		
 		// Everything In Between
-		"sw 	$t5, 0x0($a0)\n"
+		"sw 	$t5, 0x0($a0);"
 		
 		"_exitDpadCamera:"
-		"jr 	$ra\n"
-		"nop\n"
+		"nop;"
+		"jr 	$ra;"
 	);
 }
 
