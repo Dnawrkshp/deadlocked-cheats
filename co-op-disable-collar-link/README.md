d7009b6f56eb3231994eec78e6ee47deba4a54f1
Metroynome <yort124@gmail.com>
2023-01-04 16:37:20 -0500

    Now works on all Maps.

--- co-op-disable-collar-link/README.md
+++ co-op-disable-collar-link/README.md
@@ -1,5 +1,2 @@
 # co-op-disable-collar-link
 Removes the need to stay close to your co-op partner by disabling the collar link.
-
-# notes
- Maraxus nor Orxon currently work.
--- co-op-disable-collar-link/main.c
+++ co-op-disable-collar-link/main.c
@@ -1,102 +1,74 @@
 #include <tamtypes.h>
 #include <libdl/stdio.h>
-#include <libdl/game.h>
-#include <libdl/player.h>
-#include <libdl/pad.h>
 
+typedef enum
+{
+    BATTLEDOME = 1,
+    CATACROM = 2,
+    SARATHOS = 4,
+    KRONOS = 5,
+    SHAAR = 6,
+    VALIX = 7,
+    ORXON = 8,
+    TORVAL = 10,
+    STYGIA = 11,
+    MARAXUS = 13,
+    GHOST_STATION = 14,
+    DREADZONE_INTERIOR = 15
+} MAP_IDS;
 
-/*
-===CATACROM===
-
-How to Find Needed function:
- - set breakpoint at health address for map.
- - die by being too far from 2nd player
- - jr ra and go up the function.
- - jr ra the function.
-
-remove length, possible functions
-0050A338
-
-Start of Dreadzone Collar Co-op Function:
- - Jr ra to disable collar
-003FEDB0
-
-start of needed jalr v1 function: 005664B8
-address 005665C0 lw from 01B45DE8 to get 003FEDB0 (Start of co-op collar)
-
-Pointer to 01b45b40:
- - Add +A8 to get needed pointer for collar function
- - 7th pointer in list
-20289DB8
-
-Assembly loads this pointer:
-002227b4
-
-Offset between assembly loader and pointer I need:
-0x00289DB8 - 0x002227b4 = 0x67604
-
-
-===SARATHOS===
-Disable collar:
- - jr ra it
-00401490
-
-Assembly loads this pointer:
-00222734
-
-Offset between assembly loader and pointer I need:
-0x00289D24 - 0x00222734 = 0x675F0
-
-*/
-
-int GetLevel(void * PlayerPointer)
+int GetLevel(int Level)
 {
     int Collar_Func = 0;
-    switch((u32)PlayerPointer)
+    switch(Level)
     {
 		// DreadZone (Fun Fact: Containment Suit has no limitation!)
-        case 0x0036BF4C:
+        case BATTLEDOME:
             Collar_Func = 0x00492D40;
             break;
         // Catacrom
-        case 0x00368ACC:
+        case CATACROM:
             Collar_Func = 0x003FEDB0;
             break;
         // Sarathos
-        case 0x00368A4C:
+        case SARATHOS:
             Collar_Func = 0x00401490;
             break;
         // Kronos (Dark Cathedrial)
-        case 0x00368E8C:
+        case KRONOS:
             Collar_Func = 0x003E4500;
             break;
         // Shaar
-        case 0x0036898C:
+        case SHAAR:
             Collar_Func = 0x00415838;
             break;
-        // Orxon (Mining Faculty)
-        // case 0x0036898C:
-        //     Collar_Func = 0x003EEF38;
-        //     break;
         // The Valix Belt (Valix Lighthouse)
-        case 0x0036890C:
+        case VALIX:
             Collar_Func = 0x003FD4E8;
             break;
+        // Orxon (Mining Faculty)
+        case ORXON:
+            Collar_Func = 0x003EEF38;
+            break;
         // Planet Torval (Toval Ruins)
-        case 0x00368C8C:
+        case TORVAL:
             Collar_Func = 0x00466B10;
             break;
         // Stygia (Tempus Station)
-        case 0x00368A0C:
+        case STYGIA:
             Collar_Func = 0x00491AE8;
             break;
         // Maraxus (Maraxus Prison)
-        // case 0x00368C8C:
-        //     Collar_Func = 0x0047D570;
-        //     break;
+        case MARAXUS:
+            Collar_Func = 0x0047D570;
+            break;
 		// Ghost Station (Ghost Station)
-		case 0x00368B4C:
+		case GHOST_STATION:
             Collar_Func = 0x003E7978;
+            break;
+        // DreadZone Station Interior
+        case DREADZONE_INTERIOR:
+            Collar_Func = 0x003F3120;
             break;
     }
 	return Collar_Func;
@@ -109,7 +81,14 @@ int main(void)
 	if (PlayerPointer == 0 || PlayerPointer == 3451532)
 		return -1;
 
-	int a = GetLevel(PlayerPointer);
+    // if in containment suit, do not go farthur.
+    int MissionLoad = *(u32*)0x001711A8;
+    if (MissionLoad == 0x24)
+        return -1;
+
+    // Subtract 20 from current map because we are in co-op and not single player.
+    int Map = (*(u32*)0x0021DE10) - 20;
+	int a = GetLevel(Map);
 	*(u32*)(a) = 0x03e00008;
 	*(u32*)(a + 0x4) = 0;
 
