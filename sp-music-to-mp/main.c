b90ecd0df784e3498cfcac234a8c86f71b0301f0
Metroynome <yort124@gmail.com>
2023-04-03 20:06:03 -0400

    Updated Variable Names

--- sp-music-to-mp/main.c
+++ sp-music-to-mp/main.c
@@ -1,8 +1,3 @@
-/*
- sp-music-to-mp Code Created by Troy "Agent Moose" Pruitt
- Allows all single player songs to be played in Multiplayer.
-*/
-
 #include <tamtypes.h>
 #include <libdl/stdio.h>
 #include <libdl/game.h>
@@ -10,53 +5,50 @@
 #include <libdl/pad.h>
 #include <libdl/music.h>
 
-int Active = 0;
-int FinishedConvertingTracks = 0;
-int AddedTracks = 0;
+void internal_wadGetSectors(u64, u64, u64);
 
 int main(void)
 {
-	// check to see if multiplayer tracks are loaded
+	static int FinishedConvertingTracks = 0;
+	static int AddedTracks = 0;
+
 	if (!musicIsLoaded())
-	{
-		Active = 0;
 		return -1;
-	}
 
-	Active = 1;
-
-	int NewTracksLocation = 0x001CF940;
+	u32 NewTracksLocation = 0x001CF940;
 	if (!FinishedConvertingTracks || *(u32*)NewTracksLocation == 0)
 	{
 		AddedTracks = 0;
-		int MultiplayerSectorID = *(u32*)0x001CF85C;
-		int Stack = 0x0023ac00; // Original Location is 0x23ac00, but moving it somewhat fixes a bug with the SectorID.  But also, unsure if this breaks anything yet.
-		int Sector = 0x001CE470;
+		int MultiplayerSector = *(u32*)0x001CF85C;
+		int Stack = 0x0023ac00;
+		int WAD_Table = 0x001CE470;
 		int a;
 		int Offset = 0;
-		
+
 		// Zero out stack by the appropriate heap size (0x2a0 in this case)
 		// This makes sure we get the correct values we need later on.
 		memset((u32*)Stack, 0, 0x2A0);
 
-		// Loop through each Sector
+		// Loop through each WAD ID
 		for(a = 0; a < 12; a++)
 		{
 			Offset += 0x18;
-			int MapSector = *(u32*)(Sector + Offset);
+			int WAD = *(u32*)(WAD_Table + Offset);
 			// Check if Map Sector is not zero
-			if (MapSector != 0)
+			if (WAD != 0)
 			{
-				internal_wadGetSectors(MapSector, 1, Stack);
-				int SectorID = *(u32*)(Stack + 0x4);
-				if (SectorID != 0)
+				internal_wadGetSectors(WAD, 1, Stack);
+				int WAD_Sector = *(u32*)(Stack + 0x4);
+
+				// make sure WAD_Sector isn't zero
+				if (WAD_Sector != 0)
 				{
-					DPRINTF("Sector: 0x%X\n", MapSector);
-					DPRINTF("Sector ID: 0x%X\n", SectorID);
+					DPRINTF("WAD: 0x%X\n", WAD);
+					DPRINTF("WAD Sector: 0x%X\n", WAD_Sector);
 
 					// do music stuffs~
-					// Get SP 2 MP Offset for current SectorID.
-					int SP2MP = SectorID - MultiplayerSectorID;
+					// Get SP 2 MP Offset for current WAD_Sector.
+					int SP2MP = WAD_Sector - MultiplayerSector;
 					// Remember we skip the first track because it is the start of the sp track, not the body of it.
 					int b = 0;
 					int Songs = Stack + 0x18;
@@ -64,18 +56,24 @@ int main(void)
 					// if it does equal zero, that means we reached the end of the list and we move onto the next batch of tracks.
 					do
 					{
-						// Left Audio
-						int StartingSong = *(u32*)(Songs + b);
-						// Right Audio
-						int EndingSong = *(u32*)((u32)(Songs + b) + 0x8);
-						// Convert Left/Right Audio
-						int ConvertedSong_Start = SP2MP + StartingSong;
-						int ConvertedSong_End = SP2MP + EndingSong;
+						int Track_LeftAudio = *(u32*)(Songs + b);
+						int Track_RightAudio = *(u32*)((u32)(Songs + b) + 0x8);
+						int ConvertedTrack_LeftAudio = SP2MP + Track_LeftAudio;
+						int ConvertedTrack_RightAudio = SP2MP + Track_RightAudio;
 						// Apply newly Converted tracks
-						*(u32*)(NewTracksLocation) = ConvertedSong_Start;
-						*(u32*)(NewTracksLocation + 0x08) = ConvertedSong_End;
+						*(u32*)(NewTracksLocation) = ConvertedTrack_LeftAudio;
+						*(u32*)(NewTracksLocation + 0x08) = ConvertedTrack_RightAudio;
 						NewTracksLocation += 0x10;
-						b += 0x20;
+						// If on DreadZone Station, and first song, add 0x20 instead of 0x20
+						// This fixes an offset bug.
+						if (a == 0 && b == 0)
+						{
+							b += 0x28;
+						}
+						else
+						{
+							b += 0x20;
+						}
 						AddedTracks++;
 					}
 					while (*(u32*)(Songs + b) != 0);
@@ -95,55 +93,35 @@ int main(void)
 		memset((u32*)Stack, 0, 0x2A0);
 
 		FinishedConvertingTracks = 1;
-		DPRINTF("AddedTracks: %d\n", AddedTracks);
+		DPRINTF("Added Tracks: %d\n", AddedTracks);
 	};
 	
-
 	int DefaultMultiplayerTracks = 0x0d; // This number will never change
 	int StartingTrack = *(u8*)0x0021EC08;
 	int AllTracks = DefaultMultiplayerTracks + AddedTracks;
-	// Fun fact: The math for TotalTracks is exactly how the assembly is.  Didn't mean to do it that way.  (Minus the AddedTracks part)
 	int TotalTracks = (DefaultMultiplayerTracks - StartingTrack + 1) + AddedTracks;
-	int MusicDataPointer = *(u32*)0x0021DA24; // This is more than just music data pointer, but it's what Im' using it for.
+	int CodeSegmentPointer = *(u32*)0x0021DA24;
 	int CurrentTrack = *(u16*)0x00206990;
-		/*
-		MusicFunctionData was hard to find.
-		The value at 0x0021DA24 is where all the music logic first gets written too.
-		Once written, it is then copied to the needed location to run the funcion.
-		Then right when it's written, it is loaded and ran.
-		In order for the game to randomize each track (including new added tracks),
-		I had to write to the area before the finalized music function got written,
-		so it would copy my data instead of the written data.
-		If I wrote it to late, the game would only randomize the original song set for the first played song.
-		Luckly the offset for the area before it's finilized area is the same for each map.
-	*/
 	// If not in main lobby, game lobby, ect.
-	if(MusicDataPointer != 0x01430700){
-		// if Last Track doesn't equal TotalTracks
-		int MusicFunctionData = MusicDataPointer + 0x28A0D4;
-		if(*(u32*)0x0021EC0C != TotalTracks && *(u16*)MusicFunctionData != AllTracks){
+	if(CodeSegmentPointer != 0x01430700){
+		// if TRACK_RANGE_MAX doesn't equal TotalTracks
+		if(*(u32*)0x0021EC0C != TotalTracks){
+			int MusicFunctionData = CodeSegmentPointer + 0x28A0D4;
 			*(u16*)MusicFunctionData = AllTracks;
 		}
 	}
 
 	// If in game
-	if(gameIsIn())
+	if(isInGame())
 	{
 		int TrackDuration = *(u32*)0x002069A4;
 		if (*(u32*)0x002069A0 <= 0)
 		{
-			/*
-				This part: (CurrentTrack != -1 && *(u32*)0x020698C != 0)
-				fixes a bug when switching tracks, and running the command
-				to set your own track or stop a track.
-			*/
 			if ((CurrentTrack > DefaultMultiplayerTracks * 2) && (CurrentTrack != -1 && *(u32*)0x020698C != 0) && (TrackDuration <= 0x3000))
 			{
-				// This techinally cues track 1 (the shortest track) with no sound to play.
-				// Doing this lets the current playing track to fade out.
 				musicTransitionTrack(0,0,0,0);
 			}
 		}
 	}
-	return 1;
+	return 0;
 }
